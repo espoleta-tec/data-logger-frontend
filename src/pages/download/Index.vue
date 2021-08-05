a626750559242246654c1ca70fd1016a49015ac6
lazaroofarrill <lazaroofarrill@gmail.com>
2021-08-04 20:10:49 -0400

    disallow user select

--- src/pages/download/Index.vue
+++ src/pages/download/Index.vue
@@ -7,9 +7,12 @@
     <q-tab-panels class="q-card" style="min-height: 500px" v-model="currentTab">
       <q-tab-panel :key="i" :name="i" v-for="i in 6">
         <div>Informacion referente a estacion {{i}}</div>
-        <pre style="font-family: 'Raleway Medium'">
+        <pre style="font-family: 'Raleway Medium', sans-serif">
           nombre: estacion {{i}}
           version: 0.0.1
+          capacidad: 4GB
+          espacio ocupado: 300Mb
+          porciento ocupado: {{300/4000*100}}%
         </pre>
       </q-tab-panel>
     </q-tab-panels>
